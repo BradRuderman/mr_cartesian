59dfea11c6c3da75a46bb6bfb13cc08e3cc489ba
Brad Ruderman <bradruderman@gmail.com>
2014-03-08 12:20:42 -0800

    newest version

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
-hdfs dfs -rm -r /slice/output
-hadoop jar SliceOverlap.jar SliceOverlapDriver -libjars /home/cloudera/workspace/SliceOverlap/fastutil-6.5.12.jar /slice/input/ /slice/output
+hadoop jar SliceOverlap.jar SliceOverlapDriver input.txt /slice/output
 
--- src/SliceOverlapMapper.java
+++ src/SliceOverlapMapper.java
@@ -10,7 +10,6 @@ import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
-//import it.unimi.dsi.fastutil.objects.*;
 
 public class SliceOverlapMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
 
@@ -19,21 +18,12 @@ public class SliceOverlapMapper extends Mapper<LongWritable, Text, Text, IntWrit
 		String inputLine = value.toString();
 
 		ArrayList<Integer> slices = getArray(inputLine);
-		//Object2IntOpenHashMap<WritableSliceCombo> hash = new Object2IntOpenHashMap<WritableSliceCombo>();
-
+		
 	      for(int i=0;i<slices.size();i++){
 	    	  for (int j=i+1; j < slices.size() - 1; j++){
-	    		  //WritableSliceCombo innerK =  new WritableSliceCombo(slices.get(i),slices.get(j));
-	    		  //hash.addTo(innerK, 1);
 	    		  context.write(new Text(slices.get(i).toString() + "\t" + slices.get(j).toString()), new IntWritable(1));
 	    	  }
 	      };
-	      /*ObjectIterator<Entry<WritableSliceCombo, Integer>> it = hash.entrySet().iterator();
-	      while (it.hasNext()) {
-	    	  Entry<WritableSliceCombo, Integer> k = (Entry<WritableSliceCombo, Integer>)it.next();
-	    	  //System.out.println(k.getKey().toString() + "\t" + k.getValue().toString());
-	    	  context.write(new Text(k.getKey().toString()), new IntWritable(k.getValue()));
-	      }*/
 	}
 	
    private ArrayList<Integer> getArray(String value){
